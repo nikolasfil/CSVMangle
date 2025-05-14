d5c8f62c1a94b9c6f32457035be97371b189e53d
nikolasfil <nikolas13in@gmail.com>
2025-05-14 15:34:24 +0300

    updated

--- src/main.py
+++ src/main.py
@@ -18,12 +18,14 @@ class Mangler:
             # pass
         # print(lines)
 
-        new_path_properties = Path(self.folder_out, f"properties-{file_path.name}")
+        new_path_properties = Path(
+            self.folder_out, f"{file_path.name.strip('.csv')}-properties.csv"
+        )
         with open(new_path_properties, "w") as file:
             for i in range(33):
                 file.write(lines[i])
 
-        new_path_csv = Path(self.folder_out, f"csv-{file_path.name}")
+        new_path_csv = Path(self.folder_out, f"{file_path.name.strip('.csv')}-csv.csv")
         with open(new_path_csv, "w") as file:
             for i in range(33, len(lines)):
                 file.write(lines[i])
@@ -41,13 +43,15 @@ class Mangler:
             lines = file.readlines()
         return lines[20].split("Max Broadband Load       : ")[1]
 
-    def list_data_contents(self, folder_path):
+    def list_data_contents(self, folder_path, save=False):
+
+        output = []
 
         if not folder_path.exists():
             print(f"The folder {folder_path} does not exist.")
             return
 
-        for item in folder_path.iterdir():
+        for item in sorted(folder_path.iterdir()):
             if item.is_dir():
                 print(f"Directory: {item.name}")
                 for sub_item in item.iterdir():
@@ -56,13 +60,17 @@ class Mangler:
                         self.split_file(sub_item)
 
             elif item.is_file():
-                # print(f"File: {item.name}")
-                if "properties-" in item.name:
-                    # print(f"File: {item.name}")
+                print(f"Item: {item.name}")
+                if save:
+                    output.append(f"Item: {item.name}")
+                if "-properties" in item.name:
                     max_height = self.read_max_height(item)
                     print(f"Max height: {max_height}")
+                    if save:
+                        output.append(f"Max height: {max_height}")
 
-                elif "csv-" in item.name:
+                elif "-csv" in item.name:
+
                     # print(f"File: {item.name}")
                     df = self.read_csv(item)
                     df["Brd Reslt"] = pandas.to_numeric(
@@ -71,10 +79,24 @@ class Mangler:
                     # Calculate and print the mean (μέση τιμή)
                     mean_value = df["Brd Reslt"].mean()
                     print(f"Μέση τιμή (mean) της στήλης 'Brd Reslt': {mean_value:.2f}")
+                    if save:
+                        output.append(
+                            f"Μέση τιμή (mean) της στήλης 'Brd Reslt': {mean_value:.2f}"
+                        )
+
+        if save:
+            # Save the output to a text file
+            output_file = Path(self.parent, "output.txt")
+            with open(output_file, "w") as f:
+                for line in output:
+                    f.write(line + "\n")
+            print(f"Output saved to {output_file}")
 
     def main(self):
-        # self.list_data_contents(self.folder_data)
-        self.list_data_contents(self.folder_out)
+        print("Formatting files")
+        self.list_data_contents(self.folder_data)
+        print("\n\n\nSplitting files\n\n\n")
+        self.list_data_contents(self.folder_out, save=True)
 
 
 if __name__ == "__main__":
