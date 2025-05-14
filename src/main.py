8a9fc06c5fb38260758af24424668cac9700161d
nikolasfil <nikolas13in@gmail.com>
2025-05-14 16:41:05 +0300

    updated

--- src/main.py
+++ src/main.py
@@ -15,9 +15,8 @@ class Mangler:
     def split_file(self, file_path):
         with open(file_path, "r") as file:
             lines = file.readlines()
-            # pass
-        # print(lines)
 
+        # Split the properties into one file
         new_path_properties = Path(
             self.folder_out, f"{file_path.name.strip('.csv')}-properties.csv"
         )
@@ -25,6 +24,7 @@ class Mangler:
             for i in range(33):
                 file.write(lines[i])
 
+        # Split the CSV into another file
         new_path_csv = Path(self.folder_out, f"{file_path.name.strip('.csv')}-csv.csv")
         with open(new_path_csv, "w") as file:
             for i in range(33, len(lines)):
@@ -32,9 +32,7 @@ class Mangler:
 
     def read_csv(self, file_path):
         # Read a CSV file into a pandas DataFrame
-        df = pandas.read_csv(
-            file_path, skipinitialspace=True, encoding="latin-1"
-        )  # use 'latin-1' or 'cp1252' if needed
+        df = pandas.read_csv(file_path, skipinitialspace=True, encoding="latin-1")
 
         return df
 
@@ -43,15 +41,21 @@ class Mangler:
             lines = file.readlines()
         return lines[19].split("Max Broadband Resultant  : ")[1]
 
+    def print_save(self, result, lst=None, save=False):
+        if save:
+            lst.append(result)
+        print(result)
+
     def list_data_contents(self, folder_path, save=False):
         column = "Brd Load"
-        output = []
+        output_list = []
 
         if not folder_path.exists():
             print(f"The folder {folder_path} does not exist.")
             return
 
         for item in sorted(folder_path.iterdir()):
+
             if item.is_dir():
                 print(f"Directory: {item.name}")
                 for sub_item in item.iterdir():
@@ -60,43 +64,57 @@ class Mangler:
                         self.split_file(sub_item)
 
             elif item.is_file():
-                if save:
-                    output.append(f"Item: {item.name}")
+                # Used for creating the output
+
                 if "-properties" in item.name:
+
                     max_height = self.read_max_height(item)
-                    print(f"Max Broadband Resultant  : {max_height}")
-                    if save:
-                        output.append(f"Max Broadband Resultant  : {max_height}")
+
+                    self.print_save(
+                        f"Max Broadband Resultant  : {max_height}",
+                        lst=output_list,
+                        save=save,
+                    )
 
                 elif "-csv" in item.name:
 
                     item_name = (
                         item.name.strip("-properties").strip("-csv").strip(".csv")
                     )
-                    print(f"Item: {item_name}")
+
+                    self.print_save(
+                        f"Item: {item_name}",
+                        lst=output_list,
+                        save=save,
+                    )
+
                     column = "Brd Reslt"
-                    # print(f"File: {item.name}")
+
                     df = self.read_csv(item)
                     df[column] = pandas.to_numeric(df[column], errors="coerce")
+
                     # Calculate and print the mean (μέση τιμή)
                     mean_value = df[column].mean()
-                    result = f"Μέση τιμή (mean) της στήλης {column}: {mean_value}"
-                    print(result)
-                    if save:
-                        output.append(result)
+
+                    self.print_save(
+                        f"Μέση τιμή (mean) της στήλης {column}: {mean_value}",
+                        lst=output_list,
+                        save=save,
+                    )
 
                     column = "Brd Load"
                     max_value = df[column].max()
-                    result = f"Peak της στήλης {column}: {max_value:.2f}"
-                    print(result)
-                    if save:
-                        output.append(result)
+                    self.print_save(
+                        f"Peak της στήλης {column}: {max_value:.2f}",
+                        lst=output_list,
+                        save=save,
+                    )
 
         if save:
             # Save the output to a text file
             output_file = Path(self.parent, "output.txt")
             with open(output_file, "w") as f:
-                for line in output:
+                for line in output_list:
                     f.write(line + "\n")
             print(f"Output saved to {output_file}")
 
